a5f66c4220bc2b1166942e90fd4210f37d113c17
divyaarugula <divyaarugula1511@gmail.com>
2017-09-16 12:30:56 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Date e = new Date();
+
+		Student[] stu = new Student[n];
+	}
+
+}
